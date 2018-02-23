c82bb33492386ca7a93d7c00195599f3e2b58297
sanketk07 <sanketskumar@gmail.com>
2018-02-23 02:13:36 -0500

    Adding source files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+var module = angular.module("myApp", []);
+
+module.controller("MainController", ['$scope', function ($scope) {
+    $scope.data = "Sample broilerplate angular application";
+
+}]);
